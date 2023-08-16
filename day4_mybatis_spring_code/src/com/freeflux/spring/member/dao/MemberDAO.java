0d7a285316b4e4d75fa613d0038799d60ecb4285
pipipaus <pipipaus@naver.com>
2023-08-16 19:44:09 +0900

    test

---
+++ day4_mybatis_spring_code/src/com/freeflux/spring/member/dao/MemberDAO.java
@@ -0,0 +1,16 @@
+package com.freeflux.spring.member.dao;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.freeflux.spring.member.vo.MemberVO;
+
+
+public interface MemberDAO {
+	 public List selectAllMemberList() throws DataAccessException;
+	 public int insertMember(MemberVO memberVO) throws DataAccessException ;
+	 public int deleteMember(String id) throws DataAccessException;
+	 
+
+}
---
+++ day4_mybatis_spring_code/src/com/freeflux/spring/member/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.freeflux.spring.member.service;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.freeflux.spring.member.vo.MemberVO;
+
+public interface MemberService {
+	 public List listMembers() throws DataAccessException;
+	 public int addMember(MemberVO membeVO) throws DataAccessException;
+	 public int removeMember(String id) throws DataAccessException;
+
+}
